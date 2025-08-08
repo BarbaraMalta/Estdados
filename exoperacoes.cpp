c2ad1c48c4063bf8516a29f71f906d78de654e00
BARBARA MALTA MORAES <202402898892@alunos.ibmec.edu.br>
2025-08-08 09:08:50 -0300

    aula1

---
+++ exoperacoes.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // Libs Entrada/Saída C++
+using namespace std;
+// tipo nome (conjunto de parametros) assinatura da funcao
+int main(){
+    int x,y;
+    cin >> x;
+    cin >> y;
+    cout << "x = " << x+y << endl;
+    cout << "x = " << x-y << endl;
+    cout << "x = " << x/y << endl;
+    cout << "x = " << x*y << endl;
+    return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // Libs Entrada/Saída C++
+using namespace std;
+// tipo nome (conjunto de parametros) assinatura da funcao
+int main(){
+    int x = 10;
+    cout << "Valor de x é = " << x << endl;
+    cout << "Hello World!" << endl;
+    // ou
+    printf("Hello World!\n");
+
+    return 0;
+}
